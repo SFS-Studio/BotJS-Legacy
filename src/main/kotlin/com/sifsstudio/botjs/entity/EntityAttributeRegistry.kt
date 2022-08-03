90eeb21ce42dd897347ae5272b3ce6209b3c93e1
switefaster <switefaster@gmail.com>
2022-08-03 21:07:50 +0800

    refactor: rename EntityAttribute to EntityAttributeRegistry

--- src/main/kotlin/com/sifsstudio/botjs/entity/EntityAttributeRegistry.kt
+++ src/main/kotlin/com/sifsstudio/botjs/entity/EntityAttributeRegistry.kt
@@ -7,7 +7,7 @@ import net.minecraftforge.eventbus.api.SubscribeEvent
 import net.minecraftforge.fml.common.Mod
 
 @Mod.EventBusSubscriber(modid = BotJS.ID, bus = Mod.EventBusSubscriber.Bus.MOD)
-object EntityAttribute {
+object EntityAttributeRegistry {
     @SubscribeEvent
     fun attributeCreation(event: EntityAttributeCreationEvent) {
         event.put(Entities.BOT, LivingEntity.createLivingAttributes().build())

18d04f9311dd568ba3f469b2573422634f86e623
InitAuther97 <initauther97@outlook.com>
2022-08-07 11:28:14 +0800

    refactor: merged new booting logic; add item for speak ability

--- src/main/kotlin/com/sifsstudio/botjs/env/ability/SpeakAbility.kt
+++ src/main/kotlin/com/sifsstudio/botjs/env/ability/SpeakAbility.kt
@@ -1,12 +1,18 @@
 package com.sifsstudio.botjs.env.ability
 
+import net.minecraft.Util
+import net.minecraft.network.chat.ChatType
+import net.minecraft.network.chat.TextComponent
 import org.apache.logging.log4j.LogManager
 
 class SpeakAbility: AbilityBase() {
     override val id = "speak"
 
     @Suppress("unused")
-    fun speak(content: String) = LOGGER.info(content)
+    fun speak(content: String) = env.entity.server!!.playerList.broadcastMessage(TextComponent(content), ChatType.CHAT, Util.NIL_UUID)
+
+    @Suppress("unused")
+    fun log(content: String) = LOGGER.info(content)
 
     companion object {
         private val LOGGER = LogManager.getLogger("BOT")

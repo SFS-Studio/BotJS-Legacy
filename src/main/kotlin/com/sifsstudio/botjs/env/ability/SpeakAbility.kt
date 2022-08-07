14a58f82453ca0ac077db5a37349116dfcc421e9
switefaster <switefaster@gmail.com>
2022-08-07 20:59:23 +0800

    fix(ability): make SpeakAbility::speak a task
    since SpeakAbility::speak is called on EXECUTOR thread pool via javascript, it should not directly interact with game logic, but via TaskBase::tick which is running on server thread

--- src/main/kotlin/com/sifsstudio/botjs/env/ability/SpeakAbility.kt
+++ src/main/kotlin/com/sifsstudio/botjs/env/ability/SpeakAbility.kt
@@ -1,5 +1,6 @@
 package com.sifsstudio.botjs.env.ability
 
+import com.sifsstudio.botjs.env.task.TaskBase
 import net.minecraft.Util
 import net.minecraft.network.chat.ChatType
 import net.minecraft.network.chat.TextComponent
@@ -9,12 +10,21 @@ class SpeakAbility: AbilityBase() {
     override val id = "speak"
 
     @Suppress("unused")
-    fun speak(content: String) = env.entity.server!!.playerList.broadcastMessage(TextComponent(content), ChatType.CHAT, Util.NIL_UUID)
+    fun speak(content: String) = env.pending(SpeakTask(content)).join()
 
     @Suppress("unused")
     fun log(content: String) = LOGGER.info(content)
 
     companion object {
         private val LOGGER = LogManager.getLogger("BOT")
+
+        // for thread safety
+        class SpeakTask(private val content: String): TaskBase<Unit>() {
+            override fun tick() {
+                env.entity.server!!.playerList.broadcastMessage(TextComponent(content), ChatType.CHAT, Util.NIL_UUID)
+                done(Unit)
+            }
+
+        }
     }
 }
